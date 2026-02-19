2ec6ef3a507cbc71b42eacc6f341634888ecc921
fizz168 <nheanomra@gmail.com>
2026-02-19 13:01:37 +0700

    feat : exercise 5 change on app jsx and index css

--- WEEk5/Start code/exercise-5/src/App.jsx
+++ WEEk5/Start code/exercise-5/src/App.jsx
@@ -1,5 +1,51 @@
 function App() {
-  return <>{/* Your code  here */}</>;
+  return <>
+  <header>
+    <h1>My item</h1>
+  </header>
+  
+  <div className="card-view">
+    <div className="card-grid">
+      <div className="card">
+        <h4>Omra</h4>
+        <small>My daily life</small>
+        <p>
+            Try something you've always been curious about, whether it's
+            painting, cooking, coding, gardening, or playing a musical
+            instrument. Hobbies can be a great way to relax and discover new
+            passions
+        </p>
+        <img src="" alt=""></img>
+
+      </div>
+      <div className="card">
+        <h4>Seth</h4>
+        <small>My daily life</small>
+        <p>
+          Try something you've always been curious about, whether it's
+            painting, cooking, coding, gardening, or playing a musical
+            instrument. Hobbies can be a great way to relax and discover new
+            passions
+            </p>
+            <img src="asset"alt="dwhiusgo"></img>
+      </div>
+      <div class="card">
+          <h4>Dung</h4>
+          <small>My daily life</small>
+          <p>
+            Try something you've always been curious about, whether it's
+            painting, cooking, coding, gardening, or playing a musical
+            instrument. Hobbies can be a great way to relax and discover new
+            passions
+          </p>
+          <img src="" alt="the place image alt" />
+        </div>
+    </div>
+  </div>
+  </>;
+
+
+
 }
 
 export default App;
